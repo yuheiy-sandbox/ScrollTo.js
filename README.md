a8c81fee602dd42713e7dab91c265000d214ea44
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2015-08-31 05:55:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# ScrollTo.js
+A JavaScript library supports only smoothly scrolling.
+
+## Usage
+    <script src="scrollTo.min.js"></script>
+    <script>
+      scrollTo(0, 200, 'linear', function () {
+        console.log('Scrolling has ended');
+      });
+    </script>
+
+## Details
+    scrollTo([target][, duration][, easing][, callback])
+* **target**  
+Type: Number  
+A number of scroll position that the animation will move toward.
+* **duration**  
+Type: Number  
+A number determing how long the animation will run.
+* **easing**  
+Type: String  
+A string indicating which easing function to use for the transition.
+* **callback**  
+Type: Function()  
+A function to call once the animation is complete.
+
+This library corresponds to [an easing name of jQuery UI](https://jqueryui.com/easing/).  
+When you designate that, so it's possible to use it.
---
+++ src/scrollTo.js
@@ -0,0 +1,88 @@
+/*! ScrollTo.js - v.0.1.0 */
+;(function () {
+  'use strict';
+
+  const scrollTo = (target, duration, easing, callback) => {
+    callback = [target, duration, easing, callback].filter(arg => typeof arg === 'function')[0];
+
+    if (!target || typeof target !== 'number') {
+      target = 0;
+    }
+
+    if (!duration || typeof duration !== 'number') {
+      duration = 400;
+    }
+
+    if (!easing || typeof easing !== 'string') {
+      easing = 'swing';
+    }
+
+    const fps = 13;
+    const from = window.pageYOffset;
+    const diff = from - target;
+    let offset = from;
+    let elapsedTime = 0;
+
+    const tick = () => {
+      if (elapsedTime >= duration) {
+        offset = target;
+        document.documentElement.scrollTop = document.body.scrollTop = offset;
+        if (callback) callback();
+        return;
+      }
+
+      const elapsedTimeRate = elapsedTime / duration;
+      const valueChangeRate = easings[easing](elapsedTimeRate);
+      offset = from - (diff * valueChangeRate);
+      document.documentElement.scrollTop = document.body.scrollTop = offset;
+      elapsedTime += fps;
+      setTimeout(tick, fps);
+    };
+    tick();
+  };
+
+  let easings = {
+    linear:
+      p => p,
+    swing:
+      p => 0.5 - Math.cos(p * Math.PI) / 2
+  };
+
+  // Special Easings
+
+  let baseEasings = {};
+
+  ['Quad', 'Cubic', 'Quart', 'Quint', 'Expo'].forEach((name, i) =>
+    baseEasings[name] =
+      p => Math.pow(p, i + 2));
+
+  baseEasings.Sine =
+    p => 1 - Math.cos(p * Math.PI / 2);
+
+  baseEasings.Circ =
+    p => 1 - Math.sqrt(1 - p * p);
+
+  baseEasings.Elastic =
+    p => p === 0 || p === 1 ? p :
+      -Math.pow(2, 8 * (p - 1)) * Math.sin (((p - 1) * 80 - 7.5) * Math.PI / 15);
+
+  baseEasings.Back =
+    p => p * p * (3 * p - 2);
+
+  baseEasings.Bounce = p => {
+    let pow2;
+    let bounce = 4;
+    while (p < ((pow2 = Math.pow(2, --bounce)) - 1) / 11) {}
+    return 1 / Math.pow(4, 3 - bounce) - 7.5625 * Math.pow((pow2 * 3 - 2) / 22 - p, 2);
+  };
+
+  Object.keys(baseEasings).forEach(name => {
+    easings['easeIn' + name] = baseEasings[name];
+    easings['easeOut' + name] =
+      p => 1 - baseEasings[name](1 - p);
+    easings['easeInOut' + name] =
+      p => p < 0.5 ? baseEasings[name](p * 2) / 2 : 1 - baseEasings[name](p * -2 + 2) / 2;
+  });
+
+  window.scrollTo = scrollTo;
+})();
